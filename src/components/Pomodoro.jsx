4305d4f5e1b9cd6e79b658467af92be45b52b817
Freakcodes <gabhay408@gmail.com>
2023-09-20 17:07:53 +0530

    First and last commit lol

---
+++ src/components/Pomodoro.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+function Pomodoro() {
+  return (
+    <>
+        <div className='main  w-[35%] bg-blue-200 '>
+            <div className='flex gap-5'>
+                <div>Pomodoro</div>
+                <div>Short Break</div>
+                <div>Long Break</div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Pomodoro
